2878908f8d09674d8585288edcce7df018e0f403
ZZL@ZJU <3150103638@zju.edu.cn>
2017-09-29 23:32:37 +0800

    Add files via upload

--- style.js
+++ style.js
@@ -1,9 +1,25 @@
+    function showPic(whichpic)
+    {
+        var source = whichpic.getAttribute("href");
+        var placeHolder = document.getElementById("placeholder");
+        placeHolder.setAttribute("src".source);
+    }
+
+    function countBodyChindren()
+
+    {
+        var body_element = document.getElementsByTagName("body")[0];
+        alert(body_element.childNodes.length);
+    }
+
+
 
     function addWidth(name,num)
     {
       bb = document['getElementById']("colorBlock");
       bb.style[name] = num;
     }
+
     function changeColor(inputcolor)
     {
       var bb = document.getElementById("colorBlock");
@@ -27,8 +43,13 @@
       }
 
       //alert("???");
-      var num = document.getElementByTagName("li").length;
-      alert(num);
+      // var paras = document.getElementsByTagName("p");
+      // for(var i=0;i<paras.length;i++)
+      // {
+      //     var txt_title = paras[i].getAttribute("title");
+      //     if(txt_title!=null)
+      //     alert(txt_title);
+      // }
       // var num = 7.561;
       // var num = Math.round(num);
       // alert(num);
