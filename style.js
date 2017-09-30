16f45f5d54c8c96713bc9dc30714a59040258f64
ZZL@ZJU <3150103638@zju.edu.cn>
2017-09-30 15:56:08 +0800

    Add files via upload

--- style.js
+++ style.js
@@ -1,15 +1,65 @@
-    function showPic(whichpic)
+window.onload = function()
+{
+    prepareGallery();
+}
+
+function prepareGallery()
+{
+    //准备工作
+    if(!document.getElementsByTagName || !document.getElementById) return false;
+    if(!document.getElementById("imagegallery")) return false;
+    var gallery = document.getElementById("imagegallery");
+    var links = gallery.getElementsByTagName("a");
+    //准备工作done
+
+    for(var i=0;i<links.length;i++)
     {
-        var source = whichpic.getAttribute("href");
-        var placeHolder = document.getElementById("placeholder");
-        placeHolder.setAttribute("src".source);
+        links[i].onclick = function()
+        {
+            return showPic(this);
+        }
     }
+}
+
+
+function showPic(whichpic)
+    {
+        if(!document.getElementById("placeholder")) return true;
+        var source = whichpic.getAttribute("href");
+        var placeholder = document.getElementById("placeholder");
+        placeholder.setAttribute("src",source);
+
+        var text = (whichpic.getAttribute("title"))?whichpic.getAttribute("title"):"";
+        //上下等价↑↓
+        // if(whichpic.getAttribute("title"))
+        // {
+        //     var text = whichpic.getAttribute("title");
+        // }
+        // else
+        // {
+        //     var text = "";
+        // }
+
+        if(!document.getElementById("description")) return false;
+        var description = document.getElementById("description");
+        description.firstChild.nodeValue = text;
+        return false;
+    }
+
+    function popUp(url)
+    {
+        window.open(url,"new_one!!");
+    }
+
 
     function countBodyChindren()
 
     {
         var body_element = document.getElementsByTagName("body")[0];
         alert(body_element.childNodes.length);
+        var textOfP = document.getElementById("para");
+        textOfP.childNodes[0].nodeValue = "233333";
+
     }
 
 
